90209d956d515928cfc7638b1e2c20bda013c348
hyunbingil <super199g@naver.com>
2020-08-12 14:55:45 +0900

    🔧 axios

---
+++ counter-project/src/store/modules/count.js
@@ -0,0 +1,34 @@
+import { createAction, handleActions } from "redux-actions";
+import * as API from "../../lib/api";
+import produce from "immer";
+import { pender } from "redux-pender";
+
+const GET_NUM = "reducer/GET_NUM";
+const SET_NUM = "reducer/SET_NUM";
+
+export const getNum = createAction(GET_NUM, API.getNumber);
+export const setNum = createAction(SET_NUM, API.setNumber);
+
+const initialState = {
+  number: 0
+};
+
+export default handleActions(
+  {
+    ...pender({
+      type: GET_NUM,
+      onSuccess: (state, action) =>
+        produce(state, draft => {
+          draft.number = action.payload.data;
+        })
+    }),
+    ...pender({
+      type: SET_NUM,
+      onSuccess: (state, action) =>
+        produce(state, draft => {
+          draft.number = action.payload.data;
+        })
+    })
+  },
+  initialState
+);
